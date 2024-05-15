77e614b1b98c29e925f9da71eadb23da3567e7d0
felixrydberg <felixryd@gmail.com>
2024-05-15 22:52:59 +0200

    Fixed build errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
-import { positionAt } from 'node_modules/astro/dist/core/errors';
+
 // 2. Define your collection(s)
 const experiences = defineCollection({
   type: 'content',
